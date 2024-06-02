203b514c8474c0e63ee3bbcb8ec3702db0c83c4a
dean afikim <deanafikim@gmail.com>
2024-06-03 02:45:28 +0300

    added load test with k6

---
+++ load-test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '2m', target: 50 }, // ramp up to 50 users over 2 minutes
+    { duration: '3m', target: 50 }, // stay at 50 users for 3 minutes
+    { duration: '2m', target: 0 },  // ramp down to 0 users over 2 minutes
+  ],
+};
+
+export default function () {
+  http.get('http://my-app-service.default.svc.cluster.local/');
+  sleep(1);
+}
